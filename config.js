753ffad0b739fa36bfaf4f6cd253a7cd71040445
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2024-01-16 11:52:26 -0500

    Refactorisation des points d'accès de l'API, simplification des noms de route et ajout de journaux appropriés avec Winston. Amélioration de la gestion des erreurs et des messages. Mise à jour des dépendances.

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// config.js
 module.exports = {
     port: 3000,
-    apiCalypsoUrl: 'http://localhost:8080/server/api/discover/search/objects?query=calypso'
+    apiCalypsoUrl: 'http://localhost:8080/server'
 };
