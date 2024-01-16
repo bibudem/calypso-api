c0fc8742e7f360fb7c292b7a4823a4fa1ce3f980
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2024-01-16 17:00:28 -0500

    Modifier davantage de variables dans la configuration et effectuer des changements au niveau de la recherche en utilisant le scope plutôt que le collection_id.

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 module.exports = {
     port: 3000,
-    apiCalypsoUrl: 'http://localhost:8080/server'
+    apiCalypsoUrl: 'http://localhost:8080/server',
+    logsRep: 'logs/error.log',
+    vedetteGlobale: '/api/discover/search/objects?query=calypso%2Evedette%3Atrue',
+    vedetteScope: '/api/discover/search/objects?query=calypso%2Escopevedette%3Atrue'
 };
