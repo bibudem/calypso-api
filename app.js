be66730ca635e3417b91c0936d1f4929ea7393cc
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2024-01-16 11:52:26 -0500

    Refactorisation des points d'accès de l'API, simplification des noms de route et ajout de journaux appropriés avec Winston. Amélioration de la gestion des erreurs et des messages. Mise à jour des dépendances.

--- app.js
+++ app.js
@@ -1,86 +1,64 @@
 const express = require('express');
 const axios = require('axios');
-const config = require('./config'); // Importez le fichier de configuration
+const winston = require('winston');
+const config = require('./config');
+
 const app = express();
 const port = config.port;
 const apiCalypsoUrl = config.apiCalypsoUrl;
 
-// Requette specifiques
-const vedetteGlobale = '%2Evedette%3Atrue';
-const vedetteScope = '%2Escopevedette%3Atrue';
+const vedetteGlobale = '/api/discover/search/objects?query=calypso%2Evedette%3Atrue';
+const vedetteScope = '/api/discover/search/objects?query=calypso%2Escopevedette%3Atrue';
 
-// Endpoint pour récupérer les items vedettes
-app.get('/api/items/vedette', handleFeaturedItemsRequest);
+// Configuration des logs avec winston
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.File({
+            level: 'error',
+            filename: 'logs/error.log',
+            format: winston.format.combine(winston.format.timestamp(), winston.format.json())
+        })
+    ]
+});
 
-// Endpoint pour récupérer les items vedettes avec scope
-app.get('/api/items/vedette/scope', handleFeaturedScopeItemsRequest);
+// Middleware d'erreur
+app.use((err, req, res, next) => {
+    logger.error(`Erreur globale : ${err.message}`);
+    res.status(500).json({ error: 'Erreur serveur' });
+    next();
+});
 
 // Démarrer le serveur
 app.listen(port, () => {
     console.log(`Serveur écoutant sur le port ${port}`);
 });
 
-// Middleware d'erreur
-app.use((err, req, res, next) => {
-    console.error('Erreur globale :', err);
-    res.status(500).json({ error: 'Erreur serveur' });
-    next();
+// Endpoint pour récupérer les items vedettes
+app.get('/api/vedette', async (req, res) => {
+    await handleFeaturedItemsRequest(apiCalypsoUrl + vedetteGlobale, res, 'avec scope');
+});
+
+// Endpoint pour récupérer les items vedettes avec scope
+app.get('/api/vedette/scope', async (req, res) => {
+    const collectionId = req.query.collection_id;
+
+    if (!collectionId) {
+        return res.status(400).json({ error: 'Paramètre collection_id manquant' });
+    }
+
+    const apiUrl = `${apiCalypsoUrl}${vedetteScope}&collection_id=${collectionId}`;
+    await handleFeaturedItemsRequest(apiUrl, res, '');
 });
 
 // Fonction pour gérer la demande d'items vedettes
-async function handleFeaturedItemsRequest(req, res) {
+async function handleFeaturedItemsRequest(apiUrl, res, errorMessage) {
     try {
-        const featuredItems = await getFeaturedItems(apiCalypsoUrl + vedetteGlobale);
+        const featuredItems = await getFeaturedItems(apiUrl);
         res.json({ items: featuredItems });
     } catch (error) {
-        handleRequestError(res, 'Erreur lors de la récupération des données', error);
+        logger.error(`Erreur lors de la récupération des données ${errorMessage}: ${error.message}`);
+        handleRequestError(res, errorMessage, error);
     }
-}
-
-// Fonction pour gérer la demande d'items vedettes avec scope
-async function handleFeaturedScopeItemsRequest(req, res) {
-    try {
-        const collectionId = req.query.collection_id;
-
-        if (!collectionId) {
-            return res.status(400).json({ error: 'Paramètre collection_id manquant' });
-        }
-
-        const apiUrl = `${apiCalypsoUrl}${vedetteScope}&collection_id=${collectionId}`;
-        const featuredScopeItems = await getFeaturedItems(apiUrl);
-        res.json({ items: featuredScopeItems, collectionId });
-    } catch (error) {
-        handleRequestError(res, 'Erreur lors de la récupération des données avec scope', error);
-    }
-}
-
-// Fonction pour récupérer le titre, description, bundle et bitstreams des items vedettes depuis Calypso
-async function getFeaturedItems(apiUrl) {
-    const response = await axios.get(apiUrl);
-
-    if (!response.data._embedded) {
-        console.error('Aucune donnée trouvée dans la réponse de DSpace');
-        return [];
-    }
-
-    const discoverObjects = await Promise.all(
-        response.data._embedded.searchResult._embedded.objects.map(async obj => {
-            try {
-                const { idItem, group } = await getBundleInfo(obj);
-                return {
-                    id: idItem,
-                    title: obj._embedded.indexableObject.metadata['dc.title']?.[0]?.value || null,
-                    description: obj._embedded.indexableObject.metadata['dc.description']?.[0]?.value || null,
-                    group,
-                };
-            } catch (error) {
-                console.error(`Erreur lors de la récupération des informations pour l'objet ${obj._embedded.indexableObject.id}:`, error);
-                return null;
-            }
-        })
-    );
-
-    return discoverObjects.filter(item => item !== null);
 }
 
 // Fonction pour récupérer les informations du bundle depuis un objet
@@ -114,14 +92,42 @@ async function getBundleInfo(obj) {
 
         return { idItem, group };
     } catch (error) {
-        console.error(`Erreur lors de la récupération des bundles pour l'objet ${idItem}:`, error);
+        logger.error(`Erreur lors de la récupération des bundles pour l'objet ${idItem}:`, error);
         return { idItem, group: null };
     }
 }
 
+// Fonction pour récupérer les items vedettes
+async function getFeaturedItems(apiUrl) {
+    const response = await axios.get(apiUrl);
+
+    if (!response.data._embedded) {
+        console.error('Aucune donnée trouvée dans la réponse de DSpace');
+        return [];
+    }
+
+    const discoverObjects = await Promise.all(
+        response.data._embedded.searchResult._embedded.objects.map(async obj => {
+            try {
+                const { idItem, group } = await getBundleInfo(obj);
+                return {
+                    id: idItem,
+                    title: obj._embedded.indexableObject.metadata['dc.title']?.[0]?.value || null,
+                    description: obj._embedded.indexableObject.metadata['dc.description']?.[0]?.value || null,
+                    group,
+                };
+            } catch (error) {
+                logger.error(`Erreur lors de la récupération des informations pour l'objet ${obj._embedded.indexableObject.id}:`, error);
+                return null;
+            }
+        })
+    );
+
+    return discoverObjects.filter(item => item !== null);
+}
 
 // Fonction pour gérer les erreurs de requête
 function handleRequestError(res, errorMessage, error) {
-    console.error(`${errorMessage}:`, error);
+    logger.error(`${errorMessage}:`, error);
     res.status(500).json({ error: 'Erreur serveur' });
 }
