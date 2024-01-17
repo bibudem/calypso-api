daff1b95754b3a8631d00f408d13bb4d2f482136
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2024-01-17 13:00:54 -0500

    Modifications minieurs dans le nom de champ pour search.

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
     apiCalypsoUrl: 'http://localhost:8080/server',
     logsRep: 'logs/error.log',
     vedetteGlobale: '/api/discover/search/objects?query=calypso%2Evedette%3Atrue',
-    vedetteScope: '/api/discover/search/objects?query=calypso%2Escopevedette%3Atrue'
+    vedetteScope: '/api/discover/search/objects?query=calypso%2Evedettecoll%3Atrue'
 };
